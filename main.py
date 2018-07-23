a3fabfe87b530cbfc59681276d13a348fffbf20b
Nicole Wojnowski <wojnowskinicole@gmail.com>
2018-07-23 10:38:02 -0500

    added the python pages

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ import webapp2
 import logging
 import jinja2
 import os
+
 from google.appengine.api import users
-
-
-
-
-
-
-
-
 
 
 
@@ -21,10 +14,20 @@ class IndexHandler(webapp2.RequestHandler):
         logging.info('current user is %s' % (user.nickname()))
         template = jinja_env.get_template('templates/index.html')
         data = {
+        'user_nickname': user.nickname(),
           'logoutUrl': users.create_logout_url('/')
         }
         return self.response.write(template.render(data))
 
+class ScheduleHandler(webapp2.RequestHandler):
+def get(self):
+        template = jinja_env.get_template('templates/schedule.html')
+        return self.response.write(template.render())
+
+class HistoryHandler(webapp2.RequestHandler):
+def get(self):
+        template = jinja_env.get_template('templates/history.html')
+        return self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', IndexHandler),
