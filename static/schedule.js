8ccc76a0dfe6e67d86d8cb84e22c99d9e9195392
David <drod12836@gmail.com>
2018-07-24 11:41:00 -0500

    s

--- static/schedule.js
+++ static/schedule.js
@@ -6,29 +6,21 @@ fetch("http://api.worldweatheronline.com/premium/v1/weather.ashx?key=" + weather
       temp.innerHTML = "<h1>The current weather is " + currentTemp + "F<h1>"
       condition.innerHTML = "<p>" + data.data.current_condition[0].weatherDesc[0].value + "</p>"
       pic.innerHTML = "<img src='" + data.data.current_condition[0].weatherIconUrl[0].value + "'/>"
-
-      jQuery.get("/schedule?temp=" + currentTemp, () => {
+      jQuery.get("/schedule?temp=" + temp, () => {
+        alert("saved")
       })
     })
   });
-<<<<<<< HEAD
 var checkedDiv = document.querySelectorAll('#checked');
+var cool = 12
+var times = ["10AM", "12PM", "2PM", "4PM", "6PM"]
 console.log(checkedDiv)
 checkedDiv.forEach(function(element) {
-  var cool = 12
-  var inputSchedule = `<input type='checkbox'>I have drank ${cool}oz by 10AM<br>`
-  element.innerHTML = inputSchedule;
+  for(i = 0; i < times.length; i++){
+    var inputSchedule = `<input type='checkbox'>I have drank ${cool}oz by ${times[i]}<br>`
+    element.innerHTML = inputSchedule;
+  }
 });
-=======
-
-var checkedDiv = document.querySelector('#checked');
-var cool = 12
-var inputSchedule = `<input type='checkbox'>I have drank ${cool} oz by 10AM<br>`
-console.log(checkedDiv);
-checkedDiv.innerHTML = inputSchedule;
->>>>>>> 9e1162c9f658ce4dc5b372aac45b54f79703cd91
-
-
 
 //"<input type='checkbox'>I have drank"+ %s +"oz by 10AM<br>" % (12)
   // fetch("http://api.apixu.com/v1/current.json?key=" + weatherApi + "&q=60607")
