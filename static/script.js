403ac378aa770325e52cae6ab78c368cdb5cd39f
David <drod12836@gmail.com>
2018-07-24 15:35:56 -0500

    d

--- static/script.js
+++ static/script.js
@@ -1,6 +1,9 @@
 function showReminderTimes(num){
   var reminderElem = document.getElementById("reminderTimes")
   var children = reminderElem.children
+  for (var i=0; i<children.length; i++) {
+    children[i].style.visibility = "hidden";
+  }
   for (var i=0; i<num; i++) {
     children[i].style.visibility = "visible";
   }
