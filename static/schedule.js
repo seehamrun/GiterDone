268ea06455731fc0ed6073e62caea76247d82a20
David <drod12836@gmail.com>
2018-07-24 11:16:57 -0500

    a

--- static/schedule.js
+++ static/schedule.js
@@ -5,23 +5,18 @@ fetch("http://api.worldweatheronline.com/premium/v1/weather.ashx?key=" + weather
       temp = data.data.current_condition[0].temp_F
       temp.innerHTML = "<h1>The current weather is " + temp + "F<h1>"
       condition.innerHTML = "<p>" + data.data.current_condition[0].weatherDesc[0].value + "</p>"
-<<<<<<< HEAD
-      pic.innerHTML = <"img src='" + data.data.current_condition[0].weatherIconUrl[0].value + "'/>"
-    })
-  });
-var checkedDiv = document.querySelector('#checked');
-var inputSchedule = "<p>hello</p>";
-console.log(checkedDiv);
-checkedDiv.innerHTML = inputSchedule;
-=======
       pic.innerHTML = "<img src='" + data.data.current_condition[0].weatherIconUrl[0].value + "'/>"
-
       jQuery.get("/schedule?temp=" + temp, () => {
         alert("saved")
       })
-    });
-  })
->>>>>>> 55761baaad377c9c76e66bf6b168cd97844098e3
+    })
+  });
+var checkedDiv = document.querySelector('#checked');
+var cool = 12
+var inputSchedule = `<input type='checkbox'>I have drank ${cool} oz by 10AM<br>`
+console.log(checkedDiv);
+checkedDiv.innerHTML = inputSchedule;
+
 
 //"<input type='checkbox'>I have drank"+ %s +"oz by 10AM<br>" % (12)
   // fetch("http://api.apixu.com/v1/current.json?key=" + weatherApi + "&q=60607")
@@ -46,6 +41,6 @@ checkedDiv.innerHTML = inputSchedule;
 
 
   window.addEventListener('load', () => {
-    document.querySelector('#Update').addEventListener("click", submitClick)
+    document.querySelector('#submit').addEventListener("click", submitClick)
 
   });
